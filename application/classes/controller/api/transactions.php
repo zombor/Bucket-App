d67ccf4f802c1cd3c901cebafd740f071b18287a
Jeremy Bush <contractfrombelow@gmail.com>
2011-08-24 06:48:51 -0500

    Adding initial api controller for transactions

---
+++ application/classes/controller/api/transactions.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * API class for transactions
+ *
+ * @package    Bucket
+ * @author     Jeremy Bush
+ * @copyright  (c) 2011 Jeremy Bush
+ * @license    http://github.com/zombor/Bucket-App/raw/master/LICENSE
+ */
+class Controller_API_Transactions extends Controller_API
+{
+	/**
+	 * Gets a transaction
+	 *
+	 * GET /api/transaction/:id
+	 * 
+	 * @return null
+	 */
+	public function get()
+	{
+		$id = $this->request->param('id' , FALSE);
+
+		$transaction = new Model_Transaction($id);
+
+		if ( ! $transaction->loaded())
+		{
+			throw new HTTP_Exception_404('Transaction Not Found!');
+		}
+
+		$this->_response_payload = $transaction->as_array();
+	}
+
+	/**
+	 * GET /api/transactions
+	 *
+	 * @return null
+	 */
+	public function get_collection()
+	{
+		$transactions = Model::factory('transaction')->load(NULL, NULL);
+
+		$output = array();
+		foreach ($transactions as $transaction)
+		{
+			$output[] = $transaction->as_array();
+		}
+
+		$this->_response_payload = $output;
+	}
+
+	/**
+	 * POST /api/transactions
+	 *
+	 * @return null
+	 */
+	public function post_collection()
+	{
+		$transaction = new Model_Transaction;
+
+		$transaction->set_fields(
+			array(
+				'date' => arr::get($this->_request_payload, 'date'),
+				'amount' => arr::get($this->_request_payload, 'amount'),
+				'memo' => arr::get($this->_request_payload, 'memo'),
+				'cleared_status' => arr::get(
+					$this->_request_payload, 'cleared_status'
+				),
+				'payee' => arr::get($this->_request_payload, 'payee'),
+				'bucket_id' => arr::get($this->_request_payload, 'bucket_id'),
+				'account_id' => arr::get($this->_request_payload, 'account_id'),
+			)
+		);
+
+		$transaction->save();
+
+		$this->_response_payload = $transaction->as_array();
+	}
+
+	/**
+	 * PUT /api/transactions/:id
+	 *
+	 * @return null
+	 */
+	public function put()
+	{
+		$transaction = new Model_Transaction($this->request->param('id'));
+
+		if ( ! $transaction->loaded())
+		{
+			throw new HTTP_Exception_404('Transaction Not Found!');
+		}
+
+		$transaction->set_fields(
+			array(
+				'date' => arr::get($this->_request_payload, 'date'),
+				'amount' => arr::get($this->_request_payload, 'amount'),
+				'memo' => arr::get($this->_request_payload, 'memo'),
+				'cleared_status' => arr::get(
+					$this->_request_payload, 'cleared_status'
+				),
+				'payee' => arr::get($this->_request_payload, 'payee'),
+				'bucket_id' => arr::get($this->_request_payload, 'bucket_id'),
+				'account_id' => arr::get($this->_request_payload, 'account_id'),
+			)
+		);
+
+		$transaction->save();
+
+		$this->_response_payload = $transaction->as_array();
+	}
+
+	/**
+	 * DELETE /api/transactions/:id
+	 *
+	 * @return null
+	 */
+	public function delete()
+	{
+		$transaction = new Model_Transaction($this->request->param('id'));
+
+		if ( ! $transaction->loaded())
+		{
+			throw new HTTP_Exception_404('Transaction Not Found!');
+		}
+
+		$transaction->delete();
+	}
+
+	/**
+	 * DELETE /api/transactions
+	 *
+	 * @return null
+	 */
+	public function delete_collection()
+	{
+		$transactions = Model::factory('transaction')->load(NULL, NULL);
+
+		foreach ($transactions as $transaction)
+		{
+			$transaction->delete();
+		}
+	}
+}
